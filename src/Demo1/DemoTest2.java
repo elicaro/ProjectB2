e1908c75bc154b0259e89dcc82f2d11ea8749ac8
carol <carol@Carolina>
2025-01-30 10:37:49 -0500

    Logics

--- src/Demo1/DemoTest2.java
+++ src/Demo1/DemoTest2.java
@@ -1,5 +1,36 @@
 package Demo1;
 
 public class DemoTest2 {
+	
+	public static void main(String args[]) {
+
+		int dayNum = 5;
+
+		if (dayNum == 1) {
+			System.out.println("Monday");
+
+		} else if (dayNum == 2) {
+			System.out.println("Tuesday");
+
+		} else if (dayNum == 3) {
+			System.out.println("Wednesday");
+
+		} else if (dayNum == 4) {
+			System.out.println("Thursday");
+
+		} else if (dayNum == 5) {
+			System.out.println("Friday");
+
+		} else if (dayNum == 6) {
+			System.out.println("Saturday");
+
+		} else if (dayNum == 7) {
+			System.out.println("Sunday");
+
+		} else  {
+			System.out.println("wrong day");
+		}
+
+	}
 
 }
